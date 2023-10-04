ee3bea830f0f438c05cd22944ba1dca5469b93f6
Victor Alano <138694685+alanovictor@users.noreply.github.com>
2023-10-04 17:54:02 -0300

    Terminando o código

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function carregar() {
-    
     let msg = document.getElementById('msg')
     let img = document.getElementById('imagem')
     let texto = document.querySelector("p#nome")
     let data = new Date()
     let hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas. `
+    msg.innerHTML = `Agora são ${hora} horas na cidade em que você está. `
     if (hora >= 4 && hora < 12){
         //Bom dia 
         img.src = 'imagens/manha.png'
@@ -24,3 +23,53 @@ function carregar() {
             }
 }
 
+//PREVISÃO -------------------------------------------------------------
+
+//sem dotenv
+// Variáveis e seleção de elementos.
+const apiKey = "15133bb2990d7b7cdf9aea19ab4a5d78"
+const apiCountryURL = "https://countryflagsapi.com/png/"
+
+const cityInput = document.querySelector('#city-input')
+const searchBtn = document.querySelector('#search')
+
+const cityElement = document.querySelector('#city')
+const tempElement = document.querySelector('#temperature span')
+const descElement = document.querySelector('#description')
+const weatherIconElement = document.querySelector('#weather-icon')
+const countryElement = document.querySelector('#country')
+const humidityElement = document.querySelector('#humidity span')
+const windElement = document.querySelector('#wind span')
+
+
+//Funções
+const getWeatherData = async(city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`
+    const res = await fetch(apiWeatherURL)
+    const data = await res.json()
+    
+    return data
+}
+
+const showWeatherData = async (city) => { 
+const data = await getWeatherData(city)
+
+cityElement.innerText = data.name
+tempElement.innerText = parseInt(data.main.temp)
+descElement.innerText = data.weather[0].description
+weatherIconElement.setAttribute("src", `http://openweathermap.org/img/wn/${data.weather[0].icon}.png`)
+countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/shiny/64.png`) 
+humidityElement.innerText = `${data.main.humidity}%`
+windElement.innerText = `${data.wind.speed}km/h`
+}
+
+//Eventos
+searchBtn.addEventListener('click', (e) => {
+
+    e.preventDefault()
+
+    const city = cityInput.value
+
+    showWeatherData(city)
+
+})
--- scriptprev.js
+++ scriptprev.js
@@ -1,2 +1,20 @@
-//15133bb2990d7b7cdf9aea19ab4a5d78
 //sem dotenv
+// Variáveis e seleção de elementos.
+const apiKey = "15133bb2990d7b7cdf9aea19ab4a5d78"
+const apiCountryUrl = "https://countryflagsapi.com/png/"
+
+const cityInput = document.querySelector('#city-input')
+const searchBtn = document.querySelector('#search')
+
+//Funções
+
+
+//Eventos
+searchBtn.addEventListener('click', (e) => {
+
+    e.preventDefault()
+
+    console.log("teste")
+
+})
+
